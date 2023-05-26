894810e64f7197196908dcb53fbea1dc0f389abb
Shar <66898063+VN-Shar@users.noreply.github.com>
2023-05-26 13:11:12 +0700

    fix backend url

--- src/AxiosConfig.tsx
+++ src/AxiosConfig.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-	baseURL: 'http://localhost:8080/api/v1/',
-	// baseURL: 'https://mindustry-tool-backend.onrender.com/api/v1/',
+	// baseURL: 'http://localhost:8080/api/v1/',
+	baseURL: 'https://mindustry-tool-backend.onrender.com/api/v1/',
 	headers: { 'ngrok-skip-browser-warning': 'true' }
 });
