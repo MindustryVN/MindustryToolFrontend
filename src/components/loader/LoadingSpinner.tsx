5759e964b68bd35915e9c5d1a540ca178141056e
Shar <66898063+VN-Shar@users.noreply.github.com>
2023-07-21 19:36:33 +0700

    ui update

--- src/components/loader/LoadingSpinner.tsx
+++ src/components/loader/LoadingSpinner.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 export default function LoadingSpinner() {
 	return (
-		<div className='flex-center w100p'>
+		<div className='flex-center w100p medium-padding'>
 			<div className='loading-spinner' />
 		</div>
 	);
