c49ba47f8de3b3d527fe9b20d32a37a04914a776
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-01-01 09:41:42 +0700

    redirect to new web

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 export default function App() {
-	return (
-		<main className='flex h-full w-full flex-col items-center justify-center overflow-hidden'>
-			<a href='https://mindustrytool.vercel.app/'>Go to new page</a>
-		</main>
-	);
+	window.location.href = 'https://mindustry-tool.app';
+	return <main className='flex h-full w-full flex-col items-center justify-center overflow-hidden'></main>;
 }
